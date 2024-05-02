13b2b3389e5beed06338fb22d18ab6e193e21a65
Riyaz Rabbani <146787234+riyazrabbani@users.noreply.github.com>
2024-05-02 14:22:43 -0400

    updates to game board

--- 07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
+++ 07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -1,19 +1,5 @@
-const initialGameBoard = [
-    [null, null, null],
-    [null, null, null],
-    [null, null, null],
-];
 
-export default function GameBoard({onSelectSquare, turns}) {
-
-    let gameBoard = initialGameBoard;
-    for(const turn of turns) {
-        const { square, player } = turn;
-        const {row, col } = square;
-
-        gameBoard[row][col] = player;
-    }
-
+export default function GameBoard({onSelectSquare, board}) {
 
     return (
     <ol id="game-board">
@@ -22,7 +8,12 @@ export default function GameBoard({onSelectSquare, turns}) {
             <ol>
                 {row.map((playerSymbol, colIndex) => (
                 <li key={colIndex}>
-                    <button onClick = {() => onSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
+                    <button 
+                        onClick = {() => onSelectSquare(rowIndex, colIndex)} 
+                        disabled = {playerSymbol != null}
+                        >
+                        {playerSymbol}
+                    </button>
                 </li>
                 ))}
             </ol>
