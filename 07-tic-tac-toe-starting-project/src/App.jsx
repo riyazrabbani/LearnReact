b742e32d2e79112488aa96e6cb79c4fcdedb320b
Riyaz Rabbani <146787234+riyazrabbani@users.noreply.github.com>
2024-05-02 21:59:58 -0400

    Added game over screen

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react'
 import Player from './components/Player.jsx'
 import GameBoard from './components/GameBoard.jsx';
 import Log from './components/Log.jsx'
+import GameOver from './components/GameOver.jsx';
 
 import { WINNING_COMBINATIONS } from './components/winning-combinations.js';
 
@@ -32,6 +33,7 @@ function App() {
 
 
   let gameBoard = initialGameBoard;
+
   for(const turn of gameTurns) {
       const { square, player } = turn;
       const {row, col } = square;
@@ -39,15 +41,23 @@ function App() {
       gameBoard[row][col] = player;
   }
 
+    let winner;
 
   for(const combination of WINNING_COMBINATIONS) {
     const firstSquareSymbol = gameBoard[combination[0].row][combination[0].column];
     const secondSquareSymbol = gameBoard[combination[1].row][combination[1].column];
     const thirdSquareSymbol = gameBoard[combination[2].row][combination[2].column];
 
-    //left off here
-
+    if (
+      firstSquareSymbol &&
+      firstSquareSymbol === secondSquareSymbol &&
+      firstSquareSymbol === thirdSquareSymbol
+    ) {
+      winner = firstSquareSymbol;
+    }
   }
+
+  const hasDraw = gameTurns.length === 9 && !winner;
 
   function handleSelectSquare(rowIndex, colIndex) {
     //setActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X');
@@ -69,6 +79,7 @@ function App() {
           <Player initialName = "Player 1" symbol = "X" isActive={activePlayer === 'X'}/>
           <Player initialName = "Player 2" symbol = "O" isActive={activePlayer === 'O'}/>
         </ol>
+        {(winner || hasDraw) && <GameOver winner = {winner} />}
         <GameBoard 
           onSelectSquare={handleSelectSquare}
           board = {gameBoard} 
