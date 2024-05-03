0e00ef2e919026afe495b9907eed8c5057450e87
Riyaz Rabbani <146787234+riyazrabbani@users.noreply.github.com>
2024-05-03 07:58:32 -0400

    new project

--- 07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ 07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -1,13 +1,19 @@
 import { useState } from 'react'
 
-export default function Player({ initialName, symbol, isActive }) {
+export default function Player({ initialName, symbol, isActive, onChangeName }) {
     const [playerName, setPlayerName] = useState(initialName);
 
     const [isEditing, setIsEditing] = useState(false);
 
     function handleEditClick() {
         setIsEditing((editing) => !editing);
+        onChangeName(symbol, playerName)
+        
+        if(isEditing) {
+          onChangeName(symbol, playerName)
+        }
     }
+   
 
     function handleChange(event) {
       setPlayerName(event.target.value)
