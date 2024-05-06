be6051c271da7450a59671c68855b7c7822169ba
Riyaz Rabbani <146787234+riyazrabbani@users.noreply.github.com>
2024-05-05 21:01:54 -0400

    update test

--- 06-styling-react-apps/06-styling-react-apps/src/components/AuthInputs.jsx
+++ 06-styling-react-apps/06-styling-react-apps/src/components/AuthInputs.jsx
@@ -32,7 +32,7 @@ export default function AuthInputs() {
           />
         </p>
         <p>
-          <label>Password</label>
+          <label>Passphrase</label>
           <input
             type="password"
             className={passwordNotValid ? 'invalid' : undefined}
