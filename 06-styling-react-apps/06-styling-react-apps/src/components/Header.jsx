8d76596b8a2e0598fc6c3620ed9de9aeace1adb9
Riyaz Rabbani <146787234+riyazrabbani@users.noreply.github.com>
2024-05-18 14:00:48 -0400

    added tailwind

--- 06-styling-react-apps/06-styling-react-apps/src/components/Header.jsx
+++ 06-styling-react-apps/06-styling-react-apps/src/components/Header.jsx
@@ -1,12 +1,11 @@
 import logo from '../assets/logo.png';
-import classes from './Header.module.css';
 
 export default function Header() {
   return (
-    <header>
+    <header className = "flex flex-col items-center mt-8 mb-16">
       <img src={logo} alt="A canvas" />
       <h1>ReactArt</h1>
-      <p className = {classes.paragraph}>A community of artists and art-lovers.</p>
+      <p>A community of artists and art-lovers.</p>
     </header>
   );
 }
