7e2ab6f5594d749daf34bac9980ba490909f2a6f
Riyaz Rabbani <146787234+riyazrabbani@users.noreply.github.com>
2024-05-18 19:23:34 -0400

    Updating login box css

--- 06-styling-react-apps/06-styling-react-apps/src/components/AuthInputs.jsx
+++ 06-styling-react-apps/06-styling-react-apps/src/components/AuthInputs.jsx
@@ -4,13 +4,6 @@ import { styled } from 'styled-components'
 import Button from './Button.jsx'
 import Input from './Input.jsx';
 
-const ControlContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 0.5rem;
-  margin-bottom: 1.5rem;
-`
-
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState('');
   const [enteredPassword, setEnteredPassword] = useState('');
@@ -32,10 +25,10 @@ export default function AuthInputs() {
   const passwordNotValid = submitted && enteredPassword.trim().length < 6;
 
   return (
-    <div id="auth-inputs">
-      <ControlContainer>
+    <div id="auth-inputs" className = "w-full max-w-sm p-8 mx-auto rounded shadow-md bg-gradient-to-b from-stone-700 to-stone-800">
+      <div className = "flex flex-col gap-2 mb-6">
         <Input
-          label="Email"
+          label="email"
           invalid={emailNotValid}
           type="email"
           // style={{
@@ -45,20 +38,17 @@ export default function AuthInputs() {
         />
 
         <Input
-          invalid={passwordNotValid}
           label="password"
+          invalid={passwordNotValid}
           type="password"
-          className={passwordNotValid ? 'invalid' : undefined}
-          onChange={(event) =>
-            handleInputChange('password', event.target.value)
-          }
+          onChange={(event) => handleInputChange('password', event.target.value)}
         />
-      </ControlContainer>
-      <div className="actions">
-        <button type="button" className="text-button">
+      </div>
+      <div className="flex justify-end gap-4">
+        <button type="button" className="text-amber-400 hover:text-amber-500">
           Create a new account
         </button>
-        <Button className='button' onClick={handleLogin}>
+        <Button onClick={handleLogin}>
           Sign In
         </Button>
       </div>
